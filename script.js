d5d34d17ae69c0637b844ccf9f911499de614893
nhartm <natalie.hartmann98@gmail.com>
2022-09-06 12:11:44 -0700

    Added generate password function that includespassword length verifications

--- script.js
+++ script.js
@@ -22,6 +22,26 @@ function writePassword() {
   passwordText.value = password
   }
 }
+// Prompt user to determine which type of password they would like
+function generatePassword() {
+  
+  while (true) {
+    var userInput = window.prompt ("How long do you want your password to be?")
+    var passwordLength = parseInt(userInput)
+    //user cancelled prompt
+    console.log(userInput)
+    if (userInput === null) {
+    return
+    }
+    // User entered anything other than a number
+    if (isNaN(passwordLength)) {
+    window.alert("That is not a number!")
+   } else if (passwordLength < 8 || passwordLength > 128){
+    window.alert("Password must be between 8 and 128 characters long!")
+    } else {
+    break
+  }}
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
